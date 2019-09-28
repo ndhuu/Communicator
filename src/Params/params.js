5e8523170101f8538f08476ebea3bef1fb3cae06
ndhuu <danghuu.dna@gmail.com>
2019-09-29 03:01:26 +0800

    param database

---
+++ src/Params/params.js
@@ -0,0 +1,9 @@
+const USERS_DATABASE = "Users"
+const EVENTS_DATABASE = "Events"
+const SUBSCRIPTION_DATABASE = "Subcription"
+
+module.exports = {
+    USERS_DATABASE,
+    EVENTS_DATABASE,
+    SUBSCRIPTION_DATABASE
+}
